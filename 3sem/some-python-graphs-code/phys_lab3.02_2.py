027729ab1d36d542e9bc2a8d3230f7b0d7539fac
MihailPestrikov <mihail.pestrikov4dl@gmail.com>
2024-10-13 17:02:03 +0300

    .

--- 3sem/some-python-graphs-code/phys_lab3.02_2.py
+++ 3sem/some-python-graphs-code/phys_lab3.02_2.py
@@ -1,51 +1,43 @@
-import numpy as np
 import matplotlib.pyplot as plt
-from numpy import ndarray
-from scipy.interpolate import PchipInterpolator
+import numpy as np
+from scipy.interpolate import make_interp_spline
+
+x = np.array([4.56, 4.69, 4.84, 5.09, 5.2, 5.39, 5.71, 6.04, 6.38, 6.47, 7.09, 7.65, 7.79, 8.3, 9.07, 9.87, 11.07, 11.55, 12.4, 15.02])
+y_Pr = np.array([32.38, 32.88, 33.44, 34.31, 34.58, 35.25, 36.09, 36.84, 37.39, 37.59, 38.22, 38.79, 38.17, 37.93, 36.64, 34.45, 29.67, 27.03, 22.07, 1.05])
+y_Ps = np.array([14.1, 14.96, 15.93, 17.62, 18.39, 19.76, 22.17, 24.81, 27.68, 28.47, 34.18, 39.8, 41.27, 46.85, 55.94, 66.24, 83.33, 90.71, 104.56, 151.91])
+y_P = np.array([46.42, 47.74, 49.27, 51.82, 52.94, 54.87, 58.13, 61.49, 64.95, 65.86, 72.18, 77.88, 79.3, 84.49, 92.33, 100.48, 112.69, 117.58, 126.23, 152.9])
 
 
-def divide_each_array_element(a: ndarray, b: ndarray) -> ndarray:
-    y_new = np.array([])
-    for i in range(1, len(a)):
-        y_new = np.append(y_new, a[i] / b[i])
-    return y_new
+x_new = np.linspace(min(x), max(x), 100)
 
-y1 = np.array([0.2265, 0.2264, 0.2263, 0.2262, 0.2261, 0.226, 0.2259, 0.2256, 0.2225, 0.2143, 0.1868, 0.146, 0.1245, 0.1051, 0.0883, 0.0747, 0.0629, 0.0576, 0.0517, 0.0443, 0.0405, 0.0376, 0.0343, 0.0325, 0.0313])
-x1 = np.array([0.0, 0.02, 0.05, 0.07, 0.09, 0.11, 0.13, 0.15, 0.18, 0.2, 0.22, 0.24, 0.26, 0.28, 0.3, 0.32, 0.34, 0.36, 0.38, 0.4, 0.43, 0.45, 0.47, 0.49, 0.5])
-y1_new = np.array(divide_each_array_element(y1, x1))
+coefficients = np.polyfit(x, y_Pr, 2)
+poly = np.poly1d(coefficients)
+y_new_Pr = poly(x_new)
 
-y2 = np.array([0.2692, 0.2691, 0.2688, 0.2687, 0.2686, 0.2685, 0.2684, 0.2683, 0.2682, 0.2614, 0.2507, 0.1943, 0.1459, 0.1246, 0.1133, 0.1003, 0.084, 0.0702, 0.0618, 0.0543, 0.05, 0.046, 0.0431, 0.0417])
-x2 = np.array([0.0, 0.02, 0.05, 0.07, 0.09, 0.11, 0.13, 0.15, 0.17, 0.2, 0.22, 0.24, 0.26, 0.28, 0.3, 0.32, 0.35, 0.38, 0.4, 0.42, 0.45, 0.47, 0.49, 0.5])
-y2_new = np.array(divide_each_array_element(y2, x2))
+coefficients = np.polyfit(x, y_Ps, 2)
+poly = np.poly1d(coefficients)
+y_new_Ps = poly(x_new)
 
-y3 = np.array([0.3168, 0.3166, 0.3165, 0.3164, 0.3163, 0.3162, 0.3161, 0.316, 0.3153, 0.3093, 0.2925, 0.2484, 0.1951, 0.1561, 0.1373, 0.1266, 0.113, 0.1031, 0.0905, 0.083, 0.0752, 0.0677, 0.0607, 0.0579, 0.0538])
-x3 = np.array([0.0, 0.02, 0.04, 0.06, 0.08, 0.11, 0.13, 0.16, 0.18, 0.2, 0.22, 0.24, 0.26, 0.28, 0.3, 0.32, 0.34, 0.36, 0.38, 0.4, 0.42, 0.44, 0.46, 0.48, 0.5])
-y3_new = np.array(divide_each_array_element(y3, x3))
-
-y4 = np.array([0.367, 0.3669, 0.3667, 0.3664, 0.3662, 0.366, 0.3658, 0.3657, 0.3648, 0.3604, 0.3514, 0.3076, 0.2286, 0.2, 0.1676, 0.1502, 0.1396, 0.1254, 0.1139, 0.1039, 0.0957, 0.0851, 0.079, 0.0717, 0.0676])
-x4 = np.array([0.0, 0.02, 0.04, 0.06, 0.09, 0.12, 0.14, 0.17, 0.18, 0.2, 0.22, 0.24, 0.26, 0.28, 0.3, 0.32, 0.34, 0.36, 0.38, 0.4, 0.42, 0.44, 0.46, 0.48, 0.5])
-y4_new = np.array(divide_each_array_element(y4, x4))
+coefficients = np.polyfit(x, y_P, 2)
+poly = np.poly1d(coefficients)
+y_new_P = poly(x_new)
 
 
-# Интерполяция по сплайнам Эрмита
-f1 = PchipInterpolator(x1[1::], y1_new)
-f2 = PchipInterpolator(x2[1::], y2_new)
-f3 = PchipInterpolator(x3[1::], y3_new)
-f4 = PchipInterpolator(x4[1::], y4_new)
 
-x_dense1 = np.linspace(min(x1), max(x1), 1000)
-x_dense2 = np.linspace(min(x2), max(x2), 1000)
-x_dense3 = np.linspace(min(x3), max(x3), 1000)
-x_dense4 = np.linspace(min(x4), max(x4), 1000)
+plt.plot(x, y_Pr, 'o', markersize=3)
+plt.plot(x, y_Ps, 'o', markersize=3)
+plt.plot(x, y_P, 'o', markersize=3)
+plt.plot(x_new, y_new_Pr, 'b', linewidth=0.5, label=f'Полезная Pr')
+plt.plot(x_new, y_new_Ps, 'orange', linewidth=0.5, label=f'Потерь Ps')
+plt.plot(x_new, y_new_P, 'g', linewidth=0.5, label=f'Полная P')
 
 
-plt.plot(x_dense1, f1(x_dense1), color='#4682B4', label='U = 9')
-plt.plot(x_dense2, f2(x_dense2), color='#D2691E', label='U = 10.5')
-plt.plot(x_dense3, f3(x_dense3), color='#20B2AA', label='U = 12')
-plt.plot(x_dense4, f4(x_dense4), color='#FF6347', label='U = 13.5')
+plt.xlabel("сила тока I, А")
+plt.ylabel("мощность P, мВт")
+
+my_text = f"I* = {round(10.18 / (2 * 0.68), 2)} А"
+plt.scatter([], [], color="w", alpha=0, label=my_text)
+
 
 plt.legend()
-plt.xlabel("Ток соленоида, I_L, мкА")
-plt.ylabel("Отношение анодного тока к току соленоида, I_a/I_L, мкА")
 plt.show()
-
--- 3sem/some-python-graphs-code/phys_lab3.02_3.py
+++ 3sem/some-python-graphs-code/phys_lab3.02_3.py
@@ -1,20 +1,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.interpolate import make_interp_spline
 
 
+x = np.array([0, 4.56, 4.69, 4.84, 5.09, 5.2, 5.39, 5.71, 6.04, 6.38, 6.47, 7.09, 7.65, 7.79, 8.3, 9.07, 9.87, 11.07, 11.55, 12.4, 15.02])
+y = np.array([1, 0.7, 0.69, 0.68, 0.66, 0.65, 0.64, 0.62, 0.6, 0.58, 0.57, 0.53, 0.5, 0.48, 0.45, 0.4, 0.34, 0.26, 0.23, 0.17, 0.01])
 
-x = np.array([9.0000, 10.5000, 12.0000, 13.5000])
-y = np.array([0.00004762, 0.00005386, 0.00005936, 0.00006512])
 
 A = np.vstack([x, np.ones(len(x))]).T
 m, c = np.linalg.lstsq(A, y, rcond=None)[0]
 
-plt.plot(x, y, 'o', markersize=8)
-plt.plot(x, m * x + c, 'r', label=f'y = {round(m, 10)}x + {round(c, 10)}')
+plt.plot(x, y, 'o', markersize=4)
+plt.plot(x, m * x + c, 'r', label=f'y = {round(m, 2)}x + {round(c, 2)}')
 
+my_text = f"I* = 7.34 А"
+plt.scatter([], [], color="w", alpha=0, label=my_text)
+
+plt.xlabel('Сила тока I, А')
+plt.ylabel('КПД')
 
 plt.legend()
-plt.xlabel("Ток соленоида, I_L, мкА")
-plt.ylabel("Индукция магнитного поля  мкТЛ")
 plt.show()
 
